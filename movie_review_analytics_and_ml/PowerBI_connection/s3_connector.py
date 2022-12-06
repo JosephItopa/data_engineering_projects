74db8b803fb249affa41cb6f69a854394fd8ef02
Joseph Itopa <talktoitopa@gmail.com>
2022-12-06 09:50:10 +0100

    DE projects

---
+++ movie_review_analytics_and_ml/PowerBI_connection/s3_connector.py
@@ -0,0 +1,12 @@
+import boto3
+import numpy as np
+import pandas as pd
+
+AWS_KEY_ID='###'  
+AWS_SECRET='###'
+
+bucket_name = "movie-review-2022"
+
+s3_object_train = "cleaned/clean-movie-review.csv"
+obj_train = s3.get_object(Bucket=bucket_name, Key=s3_object_train)
+df_train = pd.read_csv(obj_train['Body'])
